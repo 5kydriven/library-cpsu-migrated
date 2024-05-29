d42cbf2b10b31fb4114ce164b2e150d8a890f3cf
5kydriven <tagpuno619@gmail.com>
2024-05-29 13:07:22 +0800

    added router

--- src/components/adminSidebar.vue
+++ src/components/adminSidebar.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from "vue";
-import { useRouter } from "vue-router";
+import { useRouter, RouterLink } from "vue-router";
 
 const router = useRouter();
 
@@ -12,12 +12,12 @@ const transactions = ref([
             {
                 label: 'Borrowed Books',
                 icon: 'pi pi-eraser',
-                route: '/theming'
+                route: '/borrow-books'
             },
             {
                 label: 'Return Books',
                 icon: 'pi pi-heart',
-                route: '/unstyled'
+                route: '/return-books'
             }
         ]
     }
@@ -31,12 +31,12 @@ const books = ref([
             {
                 label: 'Books List',
                 icon: 'pi pi-eraser',
-                route: '/theming'
+                route: '/books-list'
             },
             {
                 label: 'Books Location',
                 icon: 'pi pi-heart',
-                route: '/unstyled'
+                route: '/books-location'
             }
         ]
     }
@@ -50,7 +50,7 @@ const books = ref([
         <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
             <ul class="space-y-2 font-medium">
                 <li>
-                    <a href="#"
+                    <RouterLink to="/librarian"
                         class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                         <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
@@ -61,10 +61,10 @@ const books = ref([
                                 d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
                         </svg>
                         <span class="ms-3">Dashboard</span>
-                    </a>
+                    </RouterLink>
                 </li>
                 <li>
-                    <a href="#"
+                    <RouterLink to="/students-list"
                         class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                         <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
@@ -73,7 +73,7 @@ const books = ref([
                                 d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
                         </svg>
                         <span class="flex-1 ms-3 whitespace-nowrap">Students</span>
-                    </a>
+                    </RouterLink>
                 </li>
                 <li>
                     <PanelMenu :model="books">
@@ -97,7 +97,7 @@ const books = ref([
                     </PanelMenu>
                 </li>
                 <li>
-                    <a href="#"
+                    <RouterLink to="/student-logs"
                         class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                         <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
@@ -105,7 +105,7 @@ const books = ref([
                                 d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3" />
                         </svg>
                         <span class="flex-1 ms-3 whitespace-nowrap">Student log's</span>
-                    </a>
+                    </RouterLink>
                 </li>
                 <li>
                     <PanelMenu :model="transactions">
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import authPage from '../pages/authPage.vue'
+import authPage from '../pages/auth/authPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,8 +17,38 @@ const router = createRouter({
         {
           path: '',
           name: 'dashboard',
-          component: () => import('../pages/Dashboard.vue')
-        }
+          component: () => import('../pages/Dashboard.vue'),
+        },
+        {
+          path: '/students-list',
+          name: 'students-list',
+          component: () => import('../pages/StudentList.vue'),
+        },
+        {
+          path: '/books-list',
+          name: 'books-list',
+          component: () => import('../pages/books/BookList.vue'),
+        },
+        {
+          path: '/books-location',
+          name: 'books-location',
+          component: () => import('../pages/books/BookLocation.vue'),
+        },
+        {
+          path: '/student-logs',
+          name: 'student-logs',
+          component: () => import('../pages/UserLogs.vue'),
+        },
+        {
+          path: '/return-books',
+          name: 'return-books',
+          component: () => import('../pages/transactions/ReturnedBooks.vue'),
+        },
+        {
+          path: '/borrow-books',
+          name: 'borrow-books',
+          component: () => import('../pages/transactions/BorrowedBooks.vue'),
+        },
       ],
     }
   ]
