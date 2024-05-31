a7b822f6ac151f873a0f4b13f7618b4318ac650a
yoyan1 <yoyan947@gmail.com>
2024-05-31 10:50:51 -0600

    redesigning logs scanner page

--- src/pages/scanner/BorrowerLogs.vue
+++ src/pages/scanner/BorrowerLogs.vue
@@ -1,13 +1,15 @@
 <script setup>
 import {onMounted, ref} from 'vue'
 import { Html5Qrcode } from 'html5-qrcode';
+import { ProductService } from '@/service/productService'
+
 
 const scannedQrCodes = ref()
 const Html5Qrcodes = ref()
 
 const createScanQRCodes = () => {
     Html5Qrcodes.value = new Html5Qrcode("qr-code-full-region")
-    const config = {fps:10, qrbox: {width: 350, height: 350 }};
+    const config = {fps:10, qrbox: {width: 230, height: 230 }};
 
     Html5Qrcodes.value.start({facingMode:"environment" }, config, onScanSuccess)
 }
@@ -22,29 +24,46 @@ onMounted(()=>{
     createScanQRCodes()
 })
 
+
 </script>
 <template>
-    <div class=" flex items-center">
-        <Splitter style="width: 100%; height: 100%" class="mb-5">
-            <SplitterPanel class="p-10">
+    <div class="flex items-center justify-around bg-[url('/background.jpg')]" style="height: 100vh">
+            <div class="p-5 bg-white w-4/12 h-6/12 rounded-lg drop-shadow-lg">
                 <h1 class="text-2xl">Scan QR Code</h1>
                 <div id="qr-code-full-region"></div>
                 <h1 class="text-lg p-5 text-center">OR</h1>
-                <div class="flex items-center gap-10 w-full">
-                    <InputText id="username" placeholder="ex. (CPSU-LRC-0000)" v-model="value" aria-describedby="username-help" />
-                    <Button label="Submit"/>
+                <div class="flex items-center gap-5 w-full">
+                    <InputText id="username" class="w-96" placeholder="ex. (CPSU-LRC-0000)" v-model="value" aria-describedby="username-help" />
+                    <Button label="Submit" severity="info"/>
                 </div>
-            </SplitterPanel>
-            <SplitterPanel> 
-                <div>
-                    <div class="flex items-center mb-4 p-5">
+            </div>
+            <div class=" bg-white rounded-lg h-6/12 drop-shadow-sm"> 
+                <!-- <div> -->
+                    <div class="flex items-center p-5 shadow-md">
                         <img src="/logo.png" class="h-16 me-3" alt="CPSU Logo" />
                         <h1 class="text-3xl font-semibold">CPSU LIBRARY SYSTEM</h1>
                     </div>
-                    <div class="flex justify-between mt-4">
+                    <div class="flex justify-center items-center h-96">
+                        <!-- <div class="flex justify-center items-center h-80">
+                            <div class="w-32 aspect-square rounded-full relative flex justify-center items-center animate-[spin_3s_linear_infinite] z-40 bg-[conic-gradient(white_0deg,white_300deg,transparent_270deg,transparent_360deg)] before:animate-[spin_2s_linear_infinite] before:absolute before:w-[60%] before:aspect-square before:rounded-full before:z-[80] before:bg-[conic-gradient(white_0deg,white_270deg,transparent_180deg,transparent_360deg)] after:absolute after:w-3/4 after:aspect-square after:rounded-full after:z-[60] after:animate-[spin_3s_linear_infinite] after:bg-[conic-gradient(#065f46_0deg,#065f46_180deg,transparent_180deg,transparent_360deg)]">
+                                <span class="absolute w-[85%] aspect-square rounded-full z-[60] animate-[spin_5s_linear_infinite] bg-[conic-gradient(#34d399_0deg,#34d399_180deg,transparent_180deg,transparent_360deg)]">
+                                </span>
+                            </div>
+                        </div> -->
+                        <div class="w-full max-w-sm p-5 m-10 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                            <div class="flex flex-col items-center pb-10">
+                                <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="/logo.png" alt="Bonnie image"/>
+                                <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
+                                <span class="text-sm text-gray-500 dark:text-gray-400">BSIT III</span>
+                                <div class="flex gap-5 mt-4 md:mt-6">
+                                    <a href class="inline-flex items-center px-4 py-2 text-sm font-medium text-center  text-white bg-green-600 rounded-sm border">Borrow</a>
+                                    <a href class="inline-flex items-center px-4 py-2 text-sm font-medium text-center  bg-green-200 rounded-sm border border-green-600">Return</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </SplitterPanel>
-        </Splitter>
+                <!-- </div> -->
+            </div>
     </div>
 </template>
--- src/pages/scanner/StudentsLogs.vue
+++ src/pages/scanner/StudentsLogs.vue
@@ -9,7 +9,7 @@ const Html5Qrcodes = ref()
 
 const createScanQRCodes = () => {
     Html5Qrcodes.value = new Html5Qrcode("qr-code-full-region")
-    const config = {fps:10, qrbox: {width: 350, height: 350 }};
+    const config = {fps:10, qrbox: {width: 230, height: 230 }};
 
     Html5Qrcodes.value.start({facingMode:"environment" }, config, onScanSuccess)
 }
@@ -27,27 +27,43 @@ onMounted(()=>{
 
 </script>
 <template>
-    <div class=" flex items-center">
-        <Splitter style="width: 100%; height: 100%" class="mb-5">
-            <SplitterPanel class="p-10 w-6/12">
+    <div class="flex items-center justify-around bg-[url('/background.jpg')]" style="height: 100vh">
+            <div class="p-5 bg-white w-4/12 h-6/12 rounded-lg drop-shadow-lg">
                 <h1 class="text-2xl">Scan QR Code</h1>
                 <div id="qr-code-full-region"></div>
                 <h1 class="text-lg p-5 text-center">OR</h1>
-                <div class="flex items-center gap-10 w-full">
-                    <InputText id="username" placeholder="ex. (CPSU-LRC-0000)" v-model="value" aria-describedby="username-help" />
-                    <Button label="Submit"/>
+                <div class="flex items-center gap-5 w-full">
+                    <InputText id="username" class="w-96" placeholder="ex. (CPSU-LRC-0000)" v-model="value" aria-describedby="username-help" />
+                    <Button label="Submit" severity="info"/>
                 </div>
-            </SplitterPanel>
-            <SplitterPanel class="w-6/12"> 
-                <div>
-                    <div class="flex items-center mb-4 p-5 shadow-md">
+            </div>
+            <div class=" bg-white rounded-lg h-6/12 drop-shadow-sm"> 
+                <!-- <div> -->
+                    <div class="flex items-center p-5 shadow-md">
                         <img src="/logo.png" class="h-16 me-3" alt="CPSU Logo" />
                         <h1 class="text-3xl font-semibold">CPSU LIBRARY SYSTEM</h1>
                     </div>
-                        <div class="flex justify-center items-center ">
+                    <div class="flex justify-center items-center h-96">
+                        <!-- <div class="flex justify-center items-center h-80">
+                            <div class="w-32 aspect-square rounded-full relative flex justify-center items-center animate-[spin_3s_linear_infinite] z-40 bg-[conic-gradient(white_0deg,white_300deg,transparent_270deg,transparent_360deg)] before:animate-[spin_2s_linear_infinite] before:absolute before:w-[60%] before:aspect-square before:rounded-full before:z-[80] before:bg-[conic-gradient(white_0deg,white_270deg,transparent_180deg,transparent_360deg)] after:absolute after:w-3/4 after:aspect-square after:rounded-full after:z-[60] after:animate-[spin_3s_linear_infinite] after:bg-[conic-gradient(#065f46_0deg,#065f46_180deg,transparent_180deg,transparent_360deg)]">
+                                <span class="absolute w-[85%] aspect-square rounded-full z-[60] animate-[spin_5s_linear_infinite] bg-[conic-gradient(#34d399_0deg,#34d399_180deg,transparent_180deg,transparent_360deg)]">
+                                </span>
+                            </div>
+                        </div> -->
+                        <div class="w-full max-w-sm p-5 m-10 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                            <div class="flex flex-col items-center pb-10">
+                                <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="/logo.png" alt="Bonnie image"/>
+                                <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
+                                <span class="text-sm text-gray-500 dark:text-gray-400">BSIT III</span>
+                                <div class="flex mt-4 md:mt-6">
+                                    <div class="inline-flex items-center px-4 py-2 text-sm font-medium text-center  bg-green-200 rounded-sm border border-green-600">WELCOME</div>
+                                
+                                </div>
+                            </div>
                         </div>
-                </div>
-            </SplitterPanel>
-        </Splitter>
+                    </div>
+                <!-- </div> -->
+            </div>
     </div>
 </template>
