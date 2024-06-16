5e25c886a969615f15a68dcc5a489b4a18fca598
5kydriven <tagpuno619@gmail.com>
2024-06-16 10:30:51 +0800

    done test

--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -4,29 +4,29 @@ import { ref, onMounted } from 'vue'
 import { collection, addDoc } from "firebase/firestore"; 
 
 // const optColumn = ref([
-//     { field: 'title', header: 'Title' },
-//     { field: 'college', header: 'College' },
-//     { field: 'callNumber', header: 'Call Number' },
-//     { field: 'accountNumber', header: 'Account Number' },
-//     { field: 'addedEntryTitle', header: 'Added Entry Title' },
-//     { field: 'additionalSubject', header: 'Additional Subject' },
-//     { field: 'additionalTitle', header: 'Additional Title' },
-//     { field: 'author', header: 'Author' },
-//     { field: 'bibliography', header: 'Bibliography' },
-//     { field: 'copyright', header: 'Copyright' },
-//     { field: 'dealer', header: 'Dealer' },
-//     { field: 'isbn', header: 'ISBN' },
-//     { field: 'notes', header: 'Notes' },
-//     { field: 'physDesc', header: 'Physical Description' },
-//     { field: 'price', header: 'Price' },
-//     { field: 'pubPlace', header: 'Publication Place' },
-//     { field: 'publisher', header: 'Publisher' },
-//     { field: 'remarks', header: 'Remarks' },
-//     { field: 'section', header: 'Section' },
-//     { field: 'subArea', header: 'Sub Area' },
-//     { field: 'subject', header: 'Subject' },
-//     { field: 'totalPrice', header: 'Total Price' },
-//     { field: 'volume', header: 'volume' },
+//     { field: 'title', header: 'Title', isSelected: true },
+//     { field: 'college', header: 'College', isSelected: true },
+//     { field: 'callNumber', header: 'Call Number', isSelected: true },
+//     { field: 'accountNumber', header: 'Account Number', isSelected: false },
+//     { field: 'addedEntryTitle', header: 'Added Entry Title', isSelected: false },
+//     { field: 'additionalSubject', header: 'Additional Subject', isSelected: false },
+//     { field: 'additionalTitle', header: 'Additional Title', isSelected: false },
+//     { field: 'author', header: 'Author', isSelected: false },
+//     { field: 'bibliography', header: 'Bibliography', isSelected: false },
+//     { field: 'copyright', header: 'Copyright', isSelected: false },
+//     { field: 'dealer', header: 'Dealer', isSelected: false },
+//     { field: 'isbn', header: 'ISBN', isSelected: false },
+//     { field: 'notes', header: 'Notes', isSelected: false },
+//     { field: 'physDesc', header: 'Physical Description', isSelected: false },
+//     { field: 'price', header: 'Price', isSelected: false },
+//     { field: 'pubPlace', header: 'Publication Place', isSelected: false },
+//     { field: 'publisher', header: 'Publisher', isSelected: false },
+//     { field: 'remarks', header: 'Remarks', isSelected: false },
+//     { field: 'section', header: 'Section', isSelected: false },
+//     { field: 'subArea', header: 'Sub Area', isSelected: false },
+//     { field: 'subject', header: 'Subject', isSelected: false },
+//     { field: 'totalPrice', header: 'Total Price', isSelected: false },
+//     { field: 'volume', header: 'volume', isSelected: false },
 // ])
 
 // onMounted( async () => {
@@ -35,7 +35,7 @@ import { collection, addDoc } from "firebase/firestore";
 //         column: optColumn.value
 //     });
 
-//     console.log(docRef.data())
+//     console.log(added)
 // })
 
 // Add a new document in collection "columns"
