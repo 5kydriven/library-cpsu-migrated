f5d78e3b152cf2c18dfd98e6eab66d5031eb8ad8
5kydriven <tagpuno619@gmail.com>
2024-06-26 00:15:16 +0800

    first commit

---
+++ src/pages/librarian/transactions/BookLogs.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { ProductService } from '@/service/productService.js';
+
+const products = ref();
+const filters = ref();
+const dt = ref();
+
+const initFilters = () => {
+    filters.value = {
+        global: { value: null, },
+        name: { value: null },
+        category: { value: null },
+        quantity: { value: null },
+        code: { value: null },
+    };
+};
+
+initFilters();
+
+const clearFilter = () => {
+    initFilters();
+};
+
+const exportCSV = () => {
+    dt.value.exportCSV();
+};
+
+onMounted(() => {
+    ProductService.getProducts().then((data) => (products.value = data));
+});
+</script>
+<template>
+    <h1 class="text-2xl font-bold mb-4">Borrow & Return books</h1>
+    <DataTable :value="products" tableStyle="min-width: 50rem" v-model:filters="filters"
+        :globalFilterFields="['name', 'quantity', 'code', 'category']" ref="dt" removableSort showGridlines stripedRows
+        scrollable scrollHeight="400px">
+        <template #header>
+            <div class="flex justify-between">
+                <div class="flex gap-2">
+                    <Button type="button" icon="pi pi-filter-slash" label="Clear" outlined @click="clearFilter()" />
+                    <Button icon="pi pi-external-link" label="Export" @click="exportCSV($event)" />
+                </div>
+
+
+                <span class="relative">
+                    <i class="pi pi-search absolute top-2/4 -mt-2 left-3 text-surface-400 dark:text-surface-600" />
+                    <InputText v-model="filters['global'].value" placeholder="Keyword Search"
+                        class="pl-10 font-normal" />
+                </span>
+            </div>
+        </template>
+        <template #empty> No customers found. </template>
+        <Column field="code" header="Code"></Column>
+        <Column field="name" sortable header="Name"></Column>
+        <Column field="category" header="Category"></Column>
+        <Column field="quantity" header="Quantity"></Column>
+    </DataTable>
+</template>
