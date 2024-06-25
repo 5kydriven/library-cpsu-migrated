bfb0d9331d5638b16af3a6eea5c27d76b10f1f2c
5kydriven <tagpuno619@gmail.com>
2024-06-26 00:17:04 +0800

    working edit form

--- src/components/booksComp/editForm.vue
+++ src/components/booksComp/editForm.vue
@@ -1,106 +1,145 @@
 <script setup>
+import { useCrud } from '@/stores/useCrudStore.js';
+import { ref } from 'vue';
+
 const props = defineProps({
     book: Object
 });
 
+const emit = defineEmits(['result']);
 
+const store = useCrud();
+
+const book = ref(props.book);
+const loading = ref(false)
+
+const deleteBook = async () => {
+    loading.value = true
+    try {
+        await store.deleteDocument('books', book.value.uid);
+        emit('result','successDelete');
+    } catch (error) {
+        emit('result','error');
+        console.error("Failed to delete the book:", error);
+    }
+    loading.value = false
+};
+
+const updateBook = async () => {
+    loading.value = true
+    try {
+        await store.updateDocument('books', book.value.uid, book.value);
+        emit('result','successUpdate');
+    } catch (error) {
+        emit('result','error');
+        console.error("Failed to update the book:", error);
+    }
+    loading.value = false
+};
 </script>
+
 
 <template>
     <form>
         <div class="grid gap-4 mb-4 grid-cols-2">
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>Title</label>
+                <InputText v-model="book.title" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>Author</label>
+                <InputText v-model="book.author" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>Call Number</label>
+                <InputText v-model="book.callNumber" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>ISBN</label>
+                <InputText v-model="book.isbn" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>College</label>
+                <InputText v-model="book.college" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>Account Number</label>
+                <InputText v-model="book.accountNumber" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>Publisher</label>
+                <InputText v-model="book.publisher" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>Section</label>
+                <InputText v-model="book.section" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>Subject</label>
+                <InputText v-model="book.subject" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>Sub Area</label>
+                <InputText v-model="book.subArea" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>Publication Place</label>
+                <InputText v-model="book.pubPlace" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>Dealer Copyright</label>
+                <InputText v-model="book.copyright" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>Volume</label>
+                <InputText v-model="book.volume" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>Remarks</label>
+                <InputText v-model="book.remarks" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>Bibliography</label>
+                <InputText v-model="book.bibliography" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>Notes</label>
+                <InputText v-model="book.notes" />
+            </div>
+
+            <div class="col-span-1 flex flex-col gap-2">
+                <label>Additional Subject</label>
+                <InputText v-model="book.additionalSubject" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>Additional Title</label>
+                <InputText v-model="book.additionalTitle" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>Added Entry Title</label>
+                <InputText v-model="book.addedEntryTitle" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>Price</label>
+                <InputText v-model="book.price" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>Total Price</label>
+                <InputText v-model="book.totalPrice" />
             </div>
             <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label>Stocks</label>
+                <InputText v-model="book.stocks" />
             </div>
-            <div class="col-span-1 flex flex-col gap-2">
-                <label for="username">Username</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+            <div class="col-span-2 flex flex-col gap-2">
+                <label>Physical Description</label>
+                <InputText v-model="book.physDesc" />
             </div>
 
         </div>
-        <div class="w-full flex justify-end">
-            <Button label="Save" />
+        <div class="w-full flex gap-2 justify-end">
+            <Button label="Delete" severity="danger" @click="deleteBook" outlined :loading="loading" />
+            <Button label="Save" @click="updateBook" :loading="loading" />
         </div>
     </form>
 </template>
